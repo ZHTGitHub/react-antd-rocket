1980e87d14aeaaa33ee2325aeb0b61947b79adf3
ZHTGitHub <chat195@163.com>
2023-07-28 10:23:35 +0800

    perf: 添加不同大小弹窗的注解

--- src/pages/components/dialog/dialog.jsx
+++ src/pages/components/dialog/dialog.jsx
@@ -1,6 +1,18 @@
 import React from 'react'
 // import { Button } from 'antd'
 
+/**
+ * 主流显示器最大分辨率：1920 X 1080
+ * 主流显示器最小分辨率：1024 X 768
+ * 最大高度：560px
+ * 最小高度：200px
+ * n 为系统最小间距
+ * small：80 * n
+ * medium：140 * n
+ * large：180 * n
+ * x-large：240 * n
+ */
+
 const Dialog = () => {
 
   const openDialog = () => {
