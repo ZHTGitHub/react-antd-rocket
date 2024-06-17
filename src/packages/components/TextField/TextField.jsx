f5f89a791a88eccfabd1e1a21e479148554b2172
ZHT <chat195@163.com>
2024-06-17 03:16:31 +0000

    ci: 开发项目

--- src/packages/components/TextField/TextField.jsx
+++ src/packages/components/TextField/TextField.jsx
@@ -1,52 +1,51 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 // import { Input } from 'antd'
-import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
 // import { formsCreators } from 'store/forms'
 
 const TextField = (props) => {
-  const [value, setValue] = useState()
+  const [value, setValue] = useState();
 
   useEffect(() => {
-    console.log(value)
-  }, [value])
+    console.log(value);
+  }, [value]);
 
   const handleChange = (event) => {
-    const value = event.target.value
-    console.log(value)
-    console.log(props.formId)
-    console.log(props.formKey)
+    const value = event.target.value;
+    console.log(value);
+    console.log(props.formId);
+    console.log(props.formKey);
     // setValue(event.target.value)
     props.changeValue({
       formId: props.formId,
       formKey: props.formKey,
-      value
-    })
-  }
+      value,
+    });
+  };
 
   return (
     <div></div>
-    // <Input 
+    // <Input
     //   value={ value }
     //   onChange={ handleChange }
     // />
-  )
-}
+  );
+};
 
 TextField.propTypes = {
   formId: PropTypes.string,
-  formKey: PropTypes.string
-}
+  formKey: PropTypes.string,
+};
 
 const mapStateToProps = (state) => ({
-  forms: state.forms
-})
+  forms: state.forms,
+});
 
 const mapDispatchToProps = (dispatch) => ({
   changeValue(info) {
     // dispatch(formsCreators.setFormValueByKey(info))
-  }
-})
+  },
+});
 
-export default connect(mapStateToProps, mapDispatchToProps)(TextField)
-
+export default connect(mapStateToProps, mapDispatchToProps)(TextField);
